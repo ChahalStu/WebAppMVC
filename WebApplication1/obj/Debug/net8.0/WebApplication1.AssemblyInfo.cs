8abd10cb824918ecf1d50f3fbdfc9e5f5b0b2e5e
ChahalStu <218027455@stu.ukzn.ac.za>
2024-06-04 21:21:46 +0200

    removed access key

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c149f9016ef57847aae52942e3cb119ac2c67302")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e346b4ba0fc1b6d32791aa64245d4653a6ca9020")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
