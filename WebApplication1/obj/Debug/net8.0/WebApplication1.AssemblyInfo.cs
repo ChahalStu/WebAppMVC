13c297a40693acf6d8cf28145edf3870cb926244
ChahalStu <218027455@stu.ukzn.ac.za>
2024-06-04 21:44:45 +0200

    commit again

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82837cebdd24a8ee16e163ab0c8ab937030c8af3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+844b01ed4ec7afdb2620bccfe14150fb0a3fb1bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
