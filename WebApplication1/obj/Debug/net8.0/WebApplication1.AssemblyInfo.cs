409280309a0556362d78a359db6039353408dc44
ChahalStu <218027455@stu.ukzn.ac.za>
2024-06-05 14:25:29 +0200

    userroles controller

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffb51982293e2d8b97e2568d2b7bfc2838011e00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c8fa5860c7926209fd53b6dcdc17ae33cd66070")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
