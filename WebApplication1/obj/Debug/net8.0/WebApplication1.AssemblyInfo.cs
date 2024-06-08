4887d51c265f58ec9aacb47a0154a849b8530135
Lerusha <lerusha.k@gmail.com>
2024-06-08 20:57:38 +0200

    ignore list

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f361cbe39df2910ce3260b0cd66a6a252cdff75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8b6680f8311323aaa77e2906b4d1936f941a1c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
