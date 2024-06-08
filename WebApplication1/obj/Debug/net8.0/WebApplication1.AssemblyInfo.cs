de15839fd6d19f9ec6980a433edb7c0acc70ca89
Lerusha <lerusha.k@gmail.com>
2024-06-08 19:17:07 +0200

    Patients look and feel

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64b1ce073c95604b9c88d630293243826ecaedde")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfe9c82bb95e42dfdc0d9c5defeb40e44b4c99e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
