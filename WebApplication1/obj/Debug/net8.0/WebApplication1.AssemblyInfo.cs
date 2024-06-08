fb44e11cd3db31b48c9244d6d78e344b399011ed
ChahalStu <218027455@stu.ukzn.ac.za>
2024-06-08 19:45:43 +0200

    REMOVED WEBAPP 2

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfe9c82bb95e42dfdc0d9c5defeb40e44b4c99e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3efd4507fc19944aca97b192f6e605bb517418d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
