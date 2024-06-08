ced72515ed31b40a2335354741bae671ad72ee8d
ChahalStu <218027455@stu.ukzn.ac.za>
2024-06-08 22:32:18 +0200

    removed buttons

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4887d51c265f58ec9aacb47a0154a849b8530135")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ca1bb22849f6269bfd0a35a90b44e7dca261fea")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
