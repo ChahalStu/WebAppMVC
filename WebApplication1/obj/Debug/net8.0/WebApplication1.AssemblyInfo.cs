8b253d40178e18ba755d92303c4923c9445cc871
ChahalStu <218027455@stu.ukzn.ac.za>
2024-06-09 11:19:30 +0200

    Fixed navigation menu

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ca1bb22849f6269bfd0a35a90b44e7dca261fea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d30af256048252b536d32650117f47fd0420fe3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
