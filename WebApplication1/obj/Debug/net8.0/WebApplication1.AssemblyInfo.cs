ddeba6dae19c50f0c4e3a3dafaad94b62c7ebdb3
Lerusha <lerusha.k@gmail.com>
2024-06-13 20:00:23 +0200

    select 2

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5053fcdcb94ca7495dd764630be0541085849c08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1af0950fd973415f4daa935e19c8b993b8e2395d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
